3cbf06aaa8b096f6fdb36e2258a742f3da5caa79
gracexiong <1290411046@qq.com>
2017-03-03 17:58:11 +0800

    2017-3-3

--- script/member.js
+++ script/member.js
@@ -2,23 +2,34 @@
 * @Author: Marte
 * @Date:   2017-03-02 17:02:34
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-03-02 17:32:43
+* @Last Modified time: 2017-03-03 17:13:50
 */
-
 $(document).ready(function(){
-    var aLi=$("view_box li");
+    var aLi=$(".view_box ul li");
+    var oH=$(".view_box li").height();
+    var oIndex=0;
+    
     if(navigator.userAgent.toLowerCase().indexOf("firefox")!=-1){
-        document.addEventListener(DOMMouseScroll, function(){
-            if(detail<0){
-                alert("上");
+       document.addEventListener("DOMMouseScroll", function(ev){
+            var oEvent=ev||event;
+            if(oEvent.detail>0){
+                $(".view_box ul").css("top",-oIndex*oH+'px');
+                oIndex++;
+                if(oIndex>4){
+                    oIndex=4;
+                }
             }else{
-                alert("下")
+                $(".view_box ul").css("top",-oIndex*oH+'px');
+                oIndex--;
+                if(oIndex<0){
+                    oIndex=0;
+                }
             }
         }, false);
     }else{
-        document.onmouseover=function(){
-            alert(1);
-            if(wheelDelta>0){
+        document.onmousewheel=function(ev){
+            var oEvent=ev||event;
+            if(oEvent.wheelDelta>0){
                 alert("上");
             }else{
                 alert("下");
